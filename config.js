17149a982ca9ecfba7ccb8851fc74c3935129165
uff-bay <man@urbanforestfriends.org>
2026-04-19 23:40:02 -0700

    Macros for Durham

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 window.RSVP_CONFIG = {
-  webAppUrl: 'https://script.google.com/macros/s/AKfycbwFMSAK5JIULrk6HPeaVkZfTLwHDBz_FApfNlNTGcf3XUrMqOgOKohlbLOvQdlFwe9mhg/exec',
+  webAppUrl: 'https://script.google.com/macros/s/AKfycbwrVhWwguM9eI6iG6_Wl85tKCUXiVjZxOdJ7TEla6q5NgJdb8p2vK5Bwd75SsXkNHE7/exec',
 };
