53d79abd19f5c07ddc8227fa444baf3863670a1a
uff-bay <man@urbanforestfriends.org>
2026-04-20 23:15:13 -0700

    actual v3

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 window.RSVP_CONFIG = {
-  webAppUrl: 'https://script.google.com/macros/s/AKfycbwde-sqqGpSXymVcAC0Y19gH9HGEu8kioCd_SOAaFcp0kR9n_G1QqjtR0Bn1PPFxkWfMw/exec',
+  webAppUrl: 'https://script.google.com/macros/s/AKfycbwVzaVovq8UF-kbZvg2C4jIL37PaQgGvaQLaBtMAwNbF9LyNqJktysaY4m01Tf2SanU/exec',
 };
